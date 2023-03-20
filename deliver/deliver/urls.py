953ce61456bcab5062441261184c66d68234f58c
ThryLox <83971708+ThryLox@users.noreply.github.com>
2023-03-20 17:04:18 -0400

    orderingSystem_v1

--- deliver/deliver/urls.py
+++ deliver/deliver/urls.py
@@ -16,10 +16,11 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 from customer.views import Index,About
-
+from django.conf import settings
+from django.conf.urls.static import static
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', Index.as_view(),name ='index'),
     path('about/', About.as_view(),name ='about'),
 
-]
+] + static(settings.MEDIA_URL,document_root=settings.MEDIA_ROOT)
